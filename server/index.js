6b6b0624c4f86f60e3df4a3fa2f98d0229291748
himansu mallick <dreamhimansu@gmail.com>
2023-05-13 21:23:36 +0545

    login authorization

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import { error } from "console";
 import { register } from "./controllers/auth.js";
 
@@ -37,6 +38,10 @@ const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
