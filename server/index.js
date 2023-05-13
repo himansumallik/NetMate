43d657aa6f885d434978ec28e756bc26f7b76095
himansu mallick <dreamhimansu@gmail.com>
2023-05-13 16:56:35 +0545

    connected to server 3001

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const upload = multer({ storage })
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParse: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
