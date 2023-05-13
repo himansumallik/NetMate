0774bed57ffa6b2cb922743bd2c23bb2ad92a975
himansu mallick <dreamhimansu@gmail.com>
2023-05-13 16:44:01 +0545

    middleware, filkestorage and mongoose setup

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import multer from "multer";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { error } from "console";
+
+/* MIDDLEWARE CONFIGURATION*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use("/assets", express.static(path.join(__dirname,'public/assets')));
+
+/*FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function(req, res, cb){
+        cb(null, "public/storage")
+    },
+    filename: function (req, res, cb){
+        cb(null, file.originalname)
+    }
+});
+const upload = multer({ storage })
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
