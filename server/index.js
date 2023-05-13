a99eeafa118b057dec8e92f34a2a7054b7f7635a
himansu mallick <dreamhimansu@gmail.com>
2023-05-13 17:21:44 +0545

    User  model setup with mongoose

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import helmet from "helmet";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { error } from "console";
+import { register } from "./controllers/auth.js";
 
 /* MIDDLEWARE CONFIGURATION*/
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage })
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+import { Timestamp } from "mongodb";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstname: {
+            type: String,
+            required: true,
+            min: 2, 
+            max: 50,
+        },
+        lastname: {
+            type: String,
+            required: true,
+            min: 2, 
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true, 
+            max: 50,
+        },
+        password: {
+            type: String,
+            require: true,
+            min: 5,
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewdProfile : Number,
+        impressions: Number,
+
+    },
+    {Timestamp: true}
+)
+
+const User = mongoose.model("User", UserSchema)
+export default User;
