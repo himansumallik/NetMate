064fe345b80b279c5f6fdd71d102d7b1dc29ba26
himansu mallick <dreamhimansu@gmail.com>
2023-05-28 13:12:35 +0545

    components, navbar, profilepage folder created

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,7 @@
+const homepage = () => {
+    return (
+        <div>homepage</div>
+    )
+}
+
+export default homepage;
---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,7 @@
+const loginPage = () => {
+    return(
+        <div>LoginPage</div>
+    )
+}
+
+export default loginPage;
