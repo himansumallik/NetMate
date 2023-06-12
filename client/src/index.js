b601d6efb423b4f36082599c15d382d9ed001030
himansu mallick <dreamhimansu@gmail.com>
2023-06-12 11:19:33 +0545

    redux persist setup in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,34 @@ import HomePage from "scenes/homePage/index.jsx";
 import LoginPage from "scenes/loginPage/index.jsx";
 import ProfilePage from "scenes/profilePage/index.jsx";
 
+import authReducer from "./states/index.js";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -30,6 +58,10 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
 );
 
