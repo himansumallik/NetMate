6af070c06da3906faaf5dead2589c9928b85d1dc
himansu mallick <dreamhimansu@gmail.com>
2023-06-12 12:02:18 +0545

    theme and flexbeetween setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "./theme.js";
 
 function App() {
+const mode = useSelector((state) => state.mode);
+const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline />
         <Outlet />
+      </ThemeProvider> 
     </div>
   );
 }
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+})
+
+export default FlexBetween;
